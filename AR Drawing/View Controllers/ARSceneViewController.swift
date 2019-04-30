78a186a4ba165509c0a1800180c533ebc9a01da9
Shawn Ma <emailme.mx@gmail.com>
2019-04-30 12:49:58 -0400

    Add UIAlert for successfully saving video

--- AR Drawing/View Controllers/ARSceneViewController.swift
+++ AR Drawing/View Controllers/ARSceneViewController.swift
@@ -462,7 +462,14 @@ extension ARSceneViewController {
         if isRecording {
             recorder?.record()
         } else {
-            recorder?.stopAndExport()
+            recorder?.stopAndExport({ (_, _, success) in
+                if success {
+                    let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            })
         }
     }
     
