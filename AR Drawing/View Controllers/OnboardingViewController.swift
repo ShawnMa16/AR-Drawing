948a95c2d6800ffff405a5337f8294c335335631
Shawn Ma <emailme.mx@gmail.com>
2019-05-22 19:19:33 -0400

    Add alert when users denied camera access

--- AR Drawing/View Controllers/OnboardingViewController.swift
+++ AR Drawing/View Controllers/OnboardingViewController.swift
@@ -35,6 +35,7 @@ class OnboardingViewController: UIViewController {
         enableCameraButton.layer.cornerRadius = 8.0
         enableCameraButton.backgroundColor = UIColor.init(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0)
         enableCameraButton.setTitle("Enable Camera Access", for: .normal)
+        enableCameraButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         enableCameraButton.setTitleColor(.black, for: .normal)
         enableCameraButton.setTitleColor(UIColor.black.withAlphaComponent(0.3), for: .highlighted)
     }
@@ -96,9 +97,34 @@ class OnboardingViewController: UIViewController {
         switch cameraAuthorizationStatus {
         case .notDetermined: requestCameraPermission()
         case .authorized: startAR()
-        case .restricted, .denied: break
+        case .denied: showCameraSettings()
+        case .restricted: break
         default:
             break
+        }
+    }
+    
+    private func showCameraSettings() {
+        
+        let alertController = UIAlertController (title: "This app requires\ncamera access", message: "In iPhone Settings, tap AR Masters and turn on camera", preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Open Settings", style: .default) { (_) -> Void in
+            
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                return
+            }
+            
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    log.info("Settings opened: \(success)")
+                })
+            }
+        }
+        alertController.addAction(settingsAction)
+        let cancelAction = UIAlertAction(title: "Not now", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        DispatchQueue.main.async {
+            self.present(alertController, animated: true, completion: nil)
         }
     }
 }
